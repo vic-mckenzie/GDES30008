8603509c21fa10c7ef2f1219189893bbe7b6fa8a
Victoria McKenzie <vmmc@student.unimelb.edu.au>
2024-03-17 21:25:12 +1100

    moves JS location

--- JS/script.js
+++ JS/script.js
@@ -100,3 +100,13 @@ clearCanvas() {
 
 var canvas = new SignTool();
 ////////////// END DRAWING FUNCTION //////////////
+
+
+////////////// START RANDOM COLOUR //////////////
+function getRandomColor() {
+  return `hsla(${~~(360 * Math.random())}, 70%,  72%, 0.8)`
+}
+
+const randomColor = getRandomColor();
+document.documentElement.style.setProperty('--random-color', randomColor);
+////////////// END RANDOM COLOUR //////////////
