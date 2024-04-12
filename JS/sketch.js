316089726c62b5299d44f168fd084b265ee60c0d
Victoria McKenzie <vmmc@student.unimelb.edu.au>
2024-04-12 23:16:33 +1000

    working p5 background

--- JS/sketch.js
+++ JS/sketch.js
@@ -1,16 +1,23 @@
 let points = [];
-let numPoints = 10;
+let numPoints = 15;
 let currentPoint = 0;
+let lineCompleted = true;
 
 function setup() {
-  createCanvas(200, 200);
-  frameRate(13);
+  createCanvas(windowWidth, windowHeight);
+  background(0,0,0,0);
+  frameRate(10);
   generateRandomPoints();
-  stroke(random(255),random(255),random(255), random(50, 150));
-  strokeWeight(random(2,10))
+  loop();
 }
 
 function draw() {
+  if (lineCompleted) {
+    clear();
+    setTimeout(drawNewLine, 5000); // Wait for 2 seconds before drawing a new line
+    lineCompleted = false;
+  }
+
   noFill();
 
   beginShape();
@@ -19,17 +26,29 @@ function draw() {
   }
   endShape();
 
-
   if (currentPoint < numPoints) {
     currentPoint++;
   }
 }
-  
+
+function drawNewLine() {
+  stroke(random(255), random(255), random(255), random(50, 150));
+  strokeWeight(random(2, 10));
+  generateRandomPoints(); // Generate new random points for a new line
+  currentPoint = 0; // Reset currentPoint for the new line
+  lineCompleted = true; // Mark the line as completed
+}
+
 function generateRandomPoints() {
+  points = []; // Clear previous points
   for (let i = 0; i < numPoints; i++) {
-    let x = random(15, 185);
-    let y = random(15, 185);
+    let x = random(50, windowWidth - 50);
+    let y = random(50, windowHeight - 50);
     points.push(createVector(x, y));
   }
 }
 
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  generateRandomPoints(); // Regenerate points when the window is resized
+}
